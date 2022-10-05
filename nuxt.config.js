0324e7551d0f3d02668dc980ae6192e9164ec6e6
yuruli-engineer <yuruli.engineer@gmail.com>
2022-10-05 13:50:32 +0900

    sentryの追加,起動できるようにするorSaasに接続する

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,10 @@
+console.log(process.env.COMPOSE_PROJECT_NAME)
+console.log(`NODE_ENV=${process.env.NODE_ENV}`)
+console.log(`SENTRY_ENVIRONMENT=${process.env.SENTRY_ENVIRONMENT}`)
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-sentry-docker-sample',
+    title: `nuxt-sentry-docker-sample ${process.env.NODE_ENV}`,
     htmlAttrs: {
       lang: 'en'
     },
@@ -35,15 +38,73 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/sentry',
+    '@nuxtjs/proxy'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
+
+  },
+
+  sentry: {
+    // dsn: 'http://1b8d6995b0264d1ea83a526104869987@localhost:9000/2', // Enter your project's DSN here
+    dsn: 'https://16c7f37458614b21b7eef6e74fe24324@o1330068.ingest.sentry.io/6592398',
+    // Additional Module Options go here
+    // https://sentry.nuxtjs.org/sentry/options
+    config: {
+      // Add native Sentry config here
+      // https://docs.sentry.io/platforms/javascript/guides/vue/configuration/options/
+      // environment: 'local'
+    },
+    serverConfig: {
+      environment: 'local'
+    }
+    // tracing: {
+    //   tracesSampleRate: 1.0,
+    //   vueOptions: {
+    //     tracing: true
+    //   },
+    //   browserOptions: {
+    //     tracingOrigins: ['*']
+    //   }
+    // }
+  },
+
+  proxy: {
+    // '/api/': {
+    //   target: 'http://localhost:9000',
+    //   // pathRewrite: {
+    //   //   '^/api': '/'
+    //   // }
+    //   config: {
+    //     tunnel: "/tunnel"
+    //   }
+    // }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config, { isClient }) {
+      // クライアントのバンドルの webpack 設定のみを拡張する
+      if (isClient) {
+        config.devtool = 'source-map'
+      }
+      console.log(`[Build]NODE_ENV=${process.env.NODE_ENV}`)
+    }
+  },
+
+  publicRuntimeConfig: {
+    baseURL: process.env.BASE_URL || 'https://nuxtjs.org'
+  },
+
+  privateRuntimeConfig: {
+    secretKey: process.env.SECRET_KEY || 'YYYYY'
+  },
+
+  env: {
+    NODE_ENV: "local2",
   }
 }
---
+++ pages/try_sentry/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="clickErrorButton">Raise error!</button>
+</template>
+<script>
+export default {
+  async asyncData({ app }) {
+    throw Error("yuruli server3 error dayo!")
+    // const response = await app.$axios.get('https://example.com/api/blog')
+    // return {
+    //   posts: response.data
+    // }
+  },
+  mounted() {
+    console.log(`this.$config.baseURL=${this.$config.baseURL}`)
+    console.log(`this.$config.secretKey=${this.$config.secretKey}`)
+    console.log(`process.env.BASE_URL=${process.env.BASE_URL}`)
+    console.log(`process.env.SECRET_KEY=${process.env.SECRET_KEY}`)
+    console.log(`process.env.NODE_ENV=${process.env.NODE_ENV}`)
+  },
+  methods: {
+    clickErrorButton() {
+      console.log('Error button clicked')
+      throw Error("yuruli error dayo!")
+    }
+  }
+}
+</script>
